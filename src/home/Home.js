765c8ffa64c0ea3fcc0272f2b4aedd2b0dbf12c0
Balamurugan-d <balamurugan.d@ssi.safestart.com>
2020-04-11 12:24:32 +0530

    User Management Design

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import './home.scss';
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/user-list">User List</Link>
-                        </li>
-                    </ul>
-                </nav>
+
+                <div class="container">
+                    <div class="row justify-content-center align-items-center intro-section">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h3>Welcome to User management</h3>
+                                    <Link to="/user-list">
+                                        <button className="btn btn-primary">User List Screen</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/user-list/UserList.js
+++ src/user-list/UserList.js
@@ -3,7 +3,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBDataTable } from 'mdbreact';
-import { Container, Modal, Button } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import dateFormat from "dateformat";
+import "react-datepicker/dist/react-datepicker.css";
+import './userlist.scss';
+import { Modal } from "react-bootstrap";
 import { HttpInterceptor } from "../HttpInterceptor";
 
 
@@ -17,53 +21,92 @@ export default class users extends Component {
         super();
         this.state = {
             users: [],
-            show: false
+            show: false,
+            userprofile: {},
+            startDate: new Date(),
+            activeDate: [],
+            dateCount: 0
         }
     }
 
     componentDidMount() {
         HttpInterceptor.get('user-lists').then(res => {
             this.setState({ users: res.data.members })
-            debugger;
             console.log(this.state.users)
         })
 
     }
 
     handleShow = (item, e) => {
-        this.setState({ show: true })
+        let date = new Date();
+        let dateList = [];
+        let calendarDate = dateFormat(date, "mmm d yyyy");
+        let activityDate = item.activity_periods;
+        for (var i = 0; i < activityDate.length; i++) {
+            let checkDate = activityDate[i].start_time.substring(0, activityDate[i].start_time.length - 8).trim();
+            if (calendarDate === checkDate) {
+                dateList.push(activityDate[i])
+            }
+        }
+
+        this.setState({
+            show: true,
+            userprofile: item,
+            startDate: date,
+            activeDate: dateList,
+            dateCount: dateList.length
+        });
     }
 
     handleClose = () => {
         this.setState({ show: false })
     }
 
+    handleChange = date => {
+        let dateList = [];
+        let calendarDate = dateFormat(date, "mmm d yyyy");
+        let activityDate = this.state.userprofile.activity_periods;
+        for (var i = 0; i < activityDate.length; i++) {
+            let checkDate = activityDate[i].start_time.substring(0, activityDate[i].start_time.length - 8).trim();
+            if (calendarDate === checkDate) {
+                dateList.push(activityDate[i])
+            }
+        }
+        this.setState({
+            startDate: date,
+            activeDate: dateList,
+            dateCount: dateList.length
+        });
+    };
+
+
+
     render() {
+
+
 
         const data = {
             columns: [
                 {
                     label: 'Sl No',
-                    field: 'id',
-                    sort: 'asc',
+                    field: 'slno',
+                    sort: 'disabled',
                     width: 150
                 },
                 {
                     label: 'User Name',
                     field: 'real_name',
-                    sort: 'asc',
                     width: 270
                 },
                 {
                     label: 'Time Zone',
                     field: 'tz',
-                    sort: 'asc',
                     width: 200
                 },
                 {
                     label: 'Action',
                     field: 'actions',
-                    sort: 'asc',
+                    sort: 'disabled',
                     width: 100
                 },
 
@@ -73,49 +116,99 @@ export default class users extends Component {
 
         data.rows = this.state.users;
         for (let i = 0; i < this.state.users.length; i++) {
-            let action = <div className="icoBtn">
-                <i class="fas fa-edit" alt="Edit" onClick={this.handleShow.bind(this, data.rows[i])}></i>
-
-
+            let action = <div className="action-icn">
+                <i className="fas fa-eye" alt="Edit" onClick={this.handleShow.bind(this, data.rows[i])}></i>
             </div>;
+            let slno = <span>{i + 1}</span>
 
             data.rows[i]['actions'] = action;
+            data.rows[i]['slno'] = slno;
 
         }
 
         return (
-            <Container>
-                <h1>User List</h1>
-                {/* {
-                    (this.state.users.length > 0) ?
-                        <BootstrapTable keyField='id' data={this.state.users} columns={columns} />
-                        :
-                        'No Data Found'
-                } */}
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+                    <a className="navbar-brand" href="#">User management</a>
+                </nav>
 
-                <MDBDataTable
-                    striped
-                    bordered
-                    hover
-                    data={data}
-                />
+                <div className="container home-section">
+                    {
+                        (this.state.users.length > 0) ?
+                            <MDBDataTable
+                                striped
+                                bordered
+                                hover
+                                data={data}
+                            />
+                            :
+                            <div class="spinner-border text-primary alg-center" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                    }
 
-                <Modal show={this.state.show} onHide={this.handleClose} size="lg">
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                            Close
-          </Button>
-                        <Button variant="primary" onClick={this.handleClose}>
-                            Save Changes
-          </Button>
-                    </Modal.Footer>
-                </Modal>
 
-            </Container>
+
+                    <Modal show={this.state.show} onHide={this.handleClose} size="lg" className="modal-userlist">
+                        <Modal.Header closeButton>
+                            <Modal.Title>{this.state.userprofile.real_name}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div className="row">
+                                <div className="col-xl-12">
+                                    <div className="form-group">
+                                        <label>Pick Date</label>
+                                        <DatePicker className="form-control"
+                                            selected={this.state.startDate}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Name</label>
+                                        <span>{this.state.userprofile.real_name}</span>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Time Zone</label>
+                                        <span>{this.state.userprofile.tz}</span>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Total Activity Period Counts</label>
+                                        <span>{this.state.dateCount}</span>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Activity Periods List</label>
+                                        <ul>
+                                            {
+                                                this.state.activeDate.map((activeDate, i) =>
+                                                    <li>
+                                                        {activeDate.start_time}
+                                                    </li>
+                                                )
+
+                                            }
+                                            {
+                                                (this.state.dateCount === 0) ?
+                                                    <li>No Activity Date</li> : ''
+                                            }
+
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button className="btn btn-primary" onClick={this.handleClose}>
+                                Close
+          </button>
+
+                        </Modal.Footer>
+                    </Modal>
+
+                </div>
+            </div>
         )
     }
 }
